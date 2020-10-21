bfe013dc5aa5750d06bf69e30ff196c3f1f844e4
Bryan Rafsanzani <bryanrafsanzani46@gmail.com>
2020-10-21 21:50:08 +0700

    add new component Logos

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Logos></Logos>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Logos from './components/Logos.vue'
 
 export default {
   name: 'App',
   components: {
+    Logos,
     HelloWorld
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Logos.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="logos-wrapper">
+    <h2>Find Best Logo</h2>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+    <div class="item-logos">
+        <img src="" alt="">
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Logos'
+}
+</script>
+
+
+<style scoped>
+.logos-wrapper {
+  width: 70%;
+  overflow: hidden;
+  margin: auto;
+ }
+
+.item-logos {
+  width: 150px;
+  height: 150px;
+  float: left;
+  margin-right: 20px;
+  margin-bottom: 20px;
+  background: blue ;
+}
+
+</style>
